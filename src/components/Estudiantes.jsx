8e3768182be2b4d5c074f90594d0b58fcd143dd5
baltazarnavado <baltazar_n@yahoo.com>
2023-12-16 09:54:51 -0800

    ACtualizacion del Componente Estudiantes

--- src/components/Estudiantes.jsx
+++ src/components/Estudiantes.jsx
@@ -1,5 +1,16 @@
+
+import { useEffect, useState } from "react"
+
 function Estudiantes()
 {
+   useEffect(() => {
+      fetch('https://demobootcamp-vercel-api-node-postgress.vercel.app/students')
+         .then(response => response.json())
+         .then(data =>{
+            console.log(data);
+         })
+   } ,[])
+
     return (
  <>
     React and Git Working toge
