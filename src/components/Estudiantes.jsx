94a4820e535f48949549c54c5398737fd3905fc5
baltazarnavado <baltazar_n@yahoo.com>
2023-12-16 10:02:29 -0800

    Estudiantes: Se programo la presentacion de los datos que expone el api students (GET)

--- src/components/Estudiantes.jsx
+++ src/components/Estudiantes.jsx
@@ -3,17 +3,28 @@ import { useEffect, useState } from "react"
 
 function Estudiantes()
 {
+   const [dataestudiantes, setDataEstudiantes] = useState([]);
+
    useEffect(() => {
       fetch('https://demobootcamp-vercel-api-node-postgress.vercel.app/students')
          .then(response => response.json())
          .then(data =>{
             console.log(data);
+            setDataEstudiantes(data);
          })
    } ,[])
 
     return (
  <>
-    React and Git Working toge
+    <h2>Lista de Estudiantes</h2>
+    {dataestudiantes && dataestudiantes.map((item, index) => (
+      <div key={index}>
+         {item.name}
+         {item.lastname}
+      </div>
+
+    )) }
+
  </>       
     )
 
